9717ccd6c3802c2b092c3a66c02cdd96c3859992
Raina <rcho@elitedigital.ca>
2020-01-12 20:51:44 +0900

    updated with lesson 168

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -24,7 +24,8 @@ class BurgerBuilder extends Component {
             meat : 0
         },
         totalPrice: 4,
-        purchasable: false
+        purchasable: false,
+        purchasing: false
     }
 
     updatePurchaseState(ingredients){
@@ -72,6 +73,10 @@ class BurgerBuilder extends Component {
         this.updatePurchaseState();
     }
 
+    purchasingHandler = () => {
+        this.setState({purchasing: true});
+    }
+
     render() {
         const disabledInfo = {
             ...this.state.ingredients
@@ -92,7 +97,7 @@ class BurgerBuilder extends Component {
 
         return (
             <div>
-                <Modal>
+                <Modal show={this.state.purchasing}>
                     <OrderSummary ingredients = {this.state.ingredients}/>
                 </Modal>
                 <Burger ingredients = {this.state.ingredients} />
@@ -101,7 +106,8 @@ class BurgerBuilder extends Component {
                     ingredientRemoved = {this.removeIngredientHandler} 
                     disabled = {disabledInfo} 
                     purchasable = {this.state.purchasable}
-                    price={this.state.totalPrice}/>
+                    price={this.state.totalPrice}
+                    ordered={this.purchasingHandler}/>
             </div>
         );
     }

4f10a32ca44bd6c22961babbb4c3d55681d89a25
Raina <rcho@elitedigital.ca>
2020-01-26 17:31:42 +0900

    updated with lesson 179

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -4,11 +4,21 @@ import Toolbar from '../Navigation/Toolbar/Toolbar';
 import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
 
 class Layout extends Component {
+    state = {
+        showSideDrawer: true
+    }
+    
+    sideDrawerClosedHandler = () => {
+        this.setState({showSideDrawer:false});
+    }
+
     render() {
         return (
             <div>
                     <Toolbar />
-                    <SideDrawer />
+                    <SideDrawer
+                        open={this.state.showSideDrawer} 
+                        closed={this.sideDrawerClosedHandler}/>
                     <main className = {classes.Content}>
                          {this.props.children}
                     </main>
--- src/components/Navigation/SideDrawer/SideDrawer.js
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -2,18 +2,27 @@ import React from 'react';
 import Logo from '../../Logo/Logo';
 import NavigationItems from '../NavigationItems/NavigationItems';
 import classes from './SideDrawer.css';
+import Backdrop from '../../UI/Backdrop/Backdrop';
 
 const sideDrawer = (props) => {
-    // ...
+    // 조건에 따른 CSS 클라스 변경 로직
+    let attachedClasses = [classes.SideDrawer, classes.Close];
+    if(props.open){
+        attachedClasses = [classes.SideDrawer, classes.Open];
+    }
     return (
-        <div className={classes.SideDrawer}>
-            <div className = {classes.Logo}>
-                <Logo />
+        <div>
+            <Backdrop show={props.open} clicked={props.closed}/>
+            <div className={attachedClasses.join(' ')}>
+                <div className = {classes.Logo}>
+                    <Logo />
+                </div>
+                <nav>
+                    <NavigationItems />
+                </nav>    
             </div>
-            <nav>
-                <NavigationItems />
-            </nav>    
         </div>
+       
     );
 };
 

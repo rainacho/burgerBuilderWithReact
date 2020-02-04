103736af0068d3d660a334b043297d1644122fa5
raina <raina@nicebp.co.kr>
2020-02-04 17:05:31 +0900

    updated with lesson 253

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -28,6 +28,7 @@ class BurgerBuilder extends Component {
     }
 
     componentDidMount(){
+        console.log(this.props);
         axios.get('https://spring-duality-207521.firebaseio.com/ingredients.json')
             .then(response => {
                 this.setState({ingredients: response.data});
@@ -93,29 +94,30 @@ class BurgerBuilder extends Component {
 
     purchaseContinueHandler = () => {
         // alert('You Continue!');
-        this.setState({loading: true});
-        const order = {
-            ingredients:this.state.ingredients,
-            price:this.state.totalPrice,
-            customer: {
-                name: 'Raina Cho',
-                address: {
-                    street: 'Rosehill 32',
-                    zipCode: '41222',
-                    country: 'Canada'
-                },
-                email: 'test@gmail.com'
-            }
-        }
-        axios.post('/orders.json', order)
-        .then(response => {
-            alert('Ordered Successfully!');
-            this.setState({loading:false, purchasing:false});
-        })
-        .catch(error => {
-            alert('Somethin wrong!');
-            this.setState({loading:false, purchasing:false});
-        });
+        // this.setState({loading: true});
+        // const order = {
+        //     ingredients:this.state.ingredients,
+        //     price:this.state.totalPrice,
+        //     customer: {
+        //         name: 'Raina Cho',
+        //         address: {
+        //             street: 'Rosehill 32',
+        //             zipCode: '41222',
+        //             country: 'Canada'
+        //         },
+        //         email: 'test@gmail.com'
+        //     }
+        // }
+        // axios.post('/orders.json', order)
+        // .then(response => {
+        //     alert('Ordered Successfully!');
+        //     this.setState({loading:false, purchasing:false});
+        // })
+        // .catch(error => {
+        //     alert('Somethin wrong!');
+        //     this.setState({loading:false, purchasing:false});
+        // });
+        this.props.history.push('/checkout');
     }
 
     render() {
